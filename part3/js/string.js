d3953847e0188bba771edebcf319f9c76baa1574
AminaNasra <nasraaar2020@gmail.com>
2025-05-19 14:21:54 +1000

    utility functions on myString
    used utility functions on myString to find the length, first character and 11th character. outputed the result to console

--- part3/js/string.js
+++ part3/js/string.js
@@ -5,6 +5,16 @@ let hello = "Hello there!";
 let myName = "Amina";
 
 // ouputing strings
-console.log(
-  `myString: ${myString}, anotherString: ${anotherString}, hello: ${hello}, myName: ${myName}`
-);
+// console.log(
+// `myString: ${myString}, anotherString: ${anotherString}, hello: ${hello}, myName: ${myName}`
+// );
+
+// using string utility functions
+// finding the length of myString
+console.log(`length: ${myString.length}`);
+
+// finding the first character of myString
+console.log(`first character: ${myString[0]}`);
+
+// finding the 11th character of myString
+console.log(`11th character: ${myString.charAt(10)}`);
