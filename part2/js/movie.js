5ae56f1e2a1c93ab50e1abcd7bc9070476b94bbc
AminaNasra <nasraaar2020@gmail.com>
2025-05-19 12:30:18 +1000

    Change properties of myMovie object
    Changed properties of myMovie object.
    ouputed it to console

--- part2/js/movie.js
+++ part2/js/movie.js
@@ -9,5 +9,12 @@ const myMovie = {
 // Adding in propeties to myMovie object
 myMovie.Rating = 5;
 myMovie.Summary = "World War II movie about the Normandy landings";
+// output myMovie object after adding 2 properties
+// console.log(myMovie);
 
+// changing property value of myMovie object
+myMovie.Rating = 4;
+myMovie.Year = 1962;
+
+// output myMovie object after changing property data
 console.log(myMovie);
