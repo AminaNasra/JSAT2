a56403cdf82cbdd215d681e892137b9d62f2fc04
AminaNasra <nasraaar2020@gmail.com>
2025-05-19 14:01:18 +1000

    Created several strings
    defined several string variables and set it to a  value. ouputed the strings to console

---
+++ part3/js/string.js
@@ -0,0 +1,10 @@
+// Defining varias strings
+let myString = "This is a string";
+let anotherString = "   Another string";
+let hello = "Hello there!";
+let myName = "Amina";
+
+// ouputing strings
+console.log(
+  `myString: ${myString}, anotherString: ${anotherString}, hello: ${hello}, myName: ${myName}`
+);
