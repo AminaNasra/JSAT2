bcf5be553c1494d7ed110f00911eb196ca7ef66c
AminaNasra <nasraaar2020@gmail.com>
2025-05-19 12:21:31 +1000

    Update movie.js
    Added in 2 properites to myMovie object. outputed to the console

--- part2/js/movie.js
+++ part2/js/movie.js
@@ -3,5 +3,11 @@ const myMovie = {
   Title: "The longest day",
   Year: "1964",
 };
+// output myMovie object
+// console.log(myMovie);
+
+// Adding in propeties to myMovie object
+myMovie.Rating = 5;
+myMovie.Summary = "World War II movie about the Normandy landings";
 
 console.log(myMovie);
