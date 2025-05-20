c2eea6f8b4836cb7e2c7f1862e03ad70e47e7d99
AminaNasra <nasraaar2020@gmail.com>
2025-05-20 10:50:31 +1000

    Documentation
    Added  file documentation
    This documentation will be helpful for tracking modifications and understanding my implementation later.

---
+++ part3/js/files.js
@@ -0,0 +1,5 @@
+// file: files.js
+// desc: This file shows saving a text file algorith, reading a text file algorithm. writing to a text file and reading from a text file
+// Author: Amina Aar
+// Last modified: 20/05/2025
+// version 1.0.0
