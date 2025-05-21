187f1e5795f736d19d23101e1e14b15e03757f30
AminaNasra <nasraaar2020@gmail.com>
2025-05-21 11:49:15 +1000

    Created Movie class
    Declare Movie class.
    initialised fields in constructor

--- part4/js/movieClass.js
+++ part4/js/movieClass.js
@@ -3,3 +3,14 @@
 // Author: Amina Aar
 // Last modified: 20/05/2025
 // version 1.0.0
+
+// declare a Movie class
+class Movie {
+  // constructor
+  constructor(movieID, title, year, rating) {
+    this.movieID = movieID;
+    this.title = title;
+    this.year = year;
+    this.rating = rating;
+  } // end constructor
+} // end movie class
