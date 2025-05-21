77110b15b320ff9c98ff7122ffeb9911ec5ba470
AminaNasra <nasraaar2020@gmail.com>
2025-05-21 11:15:57 +1000

    part4 initial commit
    created part4, js folders, created movieClass and movieRun file s and added file comments

---
+++ part4/js/movieClass.js
@@ -0,0 +1,5 @@
+// file: movieClass.js
+// desc: This file defines the Movie class blueprint  encapsulating properties like: movieID, title, year, rating
+// Author: Amina Aar
+// Last modified: 20/05/2025
+// version 1.0.0
---
+++ part4/js/movieRun.js
@@ -0,0 +1,6 @@
+// file: movieRun.js
+// desc: This file uses the Movie class and creates a movies array  based on Movie class,
+// sorts the movies array in ascending order. Implements linearSearch function algorithm to find movieID in the input movies array.
+// Author: Amina Aar
+// Last modified: 21/05/2025
+// version 1.0.0
